0db5dfb0d72f08a08463fc9cb5a50124d35869bd
Kentaro Hayashida <kentarohayashida@hotmail.com>
2024-03-07 00:56:20 +0100

    Basic breadcrumbs implementation

--- src/shared/Breadcrumbs.jsx
+++ src/shared/Breadcrumbs.jsx
@@ -1,9 +1,24 @@
+import React from 'react';
+import { useLocation } from 'react-router-dom';
 import './Breadcrumbs.css';
 
-function Breadcrumbs(){
-	return <div id="breadcrumbs">
-		Ecological solutions / Interview, Samantha 01 / Transcribe
-	</div>
+function Breadcrumbs()
+{
+	const location = useLocation();
+	const pathnames = location.pathname.split('/').filter((x) => x);
+
+	return (
+		<div id="breadcrumbs">
+			<ul>
+				{pathnames.map((name, index) => (
+					<React.Fragment key={index * 2}>
+						<li>{name}</li>
+						{index < pathnames.length - 1 && <li>/</li>}
+					</React.Fragment>
+				))}
+			</ul>
+		</div>
+	);
 }
 
 export default Breadcrumbs;
