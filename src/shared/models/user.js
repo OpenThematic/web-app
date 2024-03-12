a8f1c8ac45f5a86fb4030db6e1538016347c1e79
Kentaro Hayashida <kentarohayashida@hotmail.com>
2024-03-12 21:39:23 +0100

    Create User model

---
+++ src/shared/models/user.js
@@ -0,0 +1,13 @@
+class User
+{
+	constructor(id, firstName, lastName, email, avatar)
+	{
+		this.id = id;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.email = email;
+		this.avatar = avatar;
+	}
+}
+
+export default User;
