eda56a9fc274b355e169b4c022f929f58034d833
Kentaro Hayashida <kentarohayashida@hotmail.com>
2024-03-17 13:17:36 +0100

    Add Button component

---
+++ src/shared/Button.tsx
@@ -0,0 +1,15 @@
+import styles from './Button.module.css';
+
+type Props = {
+	text: string;
+}
+
+const Button = ({ text }: Props) => {
+	return (
+		<button className={styles.button}>
+			{text}
+		</button>
+	);
+}
+
+export default Button;
