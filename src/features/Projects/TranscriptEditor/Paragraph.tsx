ab66ea56e2c9bbeffbad4bc4d42a78ac05fbfd13
Kentaro Hayashida <kentarohayashida@hotmail.com>
2024-05-21 22:34:35 +0200

    Suppress compiler warning

--- src/features/Projects/TranscriptEditor/Paragraph.tsx
+++ src/features/Projects/TranscriptEditor/Paragraph.tsx
@@ -1,6 +1,7 @@
 import styles from './Paragraph.module.css';
 import { formatSeconds } from '../../../shared/utils/time';
 import Word from './Word';
+import { ChangeEvent } from 'react';
 
 type Props = {
 	data: Transcript;
@@ -16,8 +17,9 @@ const Paragraph = ({ data, onTimeClick }: Props) =>
 		onTimeClick(data.start);
 	};
 
-	const handleChange = () =>
+	const handleChange = (event: ChangeEvent<HTMLParagraphElement>) =>
 	{
+		console.log(event.target.innerText);
 		// TODO: When editing text, check if word is entirely deleted or replaced
 	};
 
@@ -25,7 +27,7 @@ const Paragraph = ({ data, onTimeClick }: Props) =>
 		<div className={styles.paragraph}>
 			<p className={styles.time} onClick={handleTimeClick}>{formatSeconds(data.start)}</p>
 			<p className={styles.speaker}>{data.speaker}</p>
-			<p className={styles.text} contentEditable>
+			<p className={styles.text} onInput={handleChange} contentEditable suppressContentEditableWarning>
 				{data.words.map((word: Word) =>
 					<>
 						<Word data={word} />
