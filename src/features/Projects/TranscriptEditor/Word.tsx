2b88a05e295cef26c0cadced5e0a52413c0277a5
Kentaro Hayashida <kentarohayashida@hotmail.com>
2024-05-27 23:50:37 +0200

    Remove magic number

--- src/features/Projects/TranscriptEditor/Word.tsx
+++ src/features/Projects/TranscriptEditor/Word.tsx
@@ -6,8 +6,10 @@ type Props = {
 
 const Word = ({ data }: Props) =>
 {
+	const lowConfidenceThreshold = .3;
+
 	return (
-		data.probability < .3 ? (
+		data.probability < lowConfidenceThreshold ? (
 			<span className={styles.word + ' ' + styles.lowConfidence} contentEditable >{data.word.trim()}</span>
 		) : (
 			<span className={styles.word}>{data.word.trim()}</span>
